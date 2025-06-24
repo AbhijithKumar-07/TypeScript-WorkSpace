3276d38fb4969ba0c957a80406b7540c3c782024
AbhijithKumar-07 <abhiyandrapu1106@gmail.com>
2025-06-24 23:40:28 +0530

    change

--- 3. Function Parameter Annotations/Function Parameter Annotations.ts
+++ 3. Function Parameter Annotations/Function Parameter Annotations.ts
@@ -1,1 +1,24 @@
+// Regular Function
+function addOne(age: number) {
+    return age + 1;
+}
+const result = addOne(19);
+console.log(result);
 
+
+// Arrow Function
+let multiply = (x: number, y: number) => {
+    return x*y;
+}
+const product = multiply(9,8);
+console.log(product);
+
+
+// Default Parameters
+function greet(person: string = "UnKnown") {
+    return `Hello ${person}`
+}
+let p1 = greet();
+console.log(p1);
+let p2 = greet("Abhi");
+console.log(p2);
