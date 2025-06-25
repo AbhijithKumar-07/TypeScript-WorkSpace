9600539e5947615f8865c7dc1d1a4b3481f92104
AbhijithKumar-07 <abhiyandrapu1106@gmail.com>
2025-06-25 22:57:10 +0530

    change

--- 10. Type Aliases/type_aliases.ts
+++ 10. Type Aliases/type_aliases.ts
@@ -2,15 +2,46 @@
 // Here we are providing 'dataType_of_value' -->> It is known as 'Annotation'
 //Example 1
 let n: number = 7;
-// Here we are providing the dataType of variable n 
+// Here we are providing the dataType of variable n
 // -->> So, the 'dataType' of the variable we are providing is known as "Annotation"
+// Here In the above example --> number --> Is "Annotation"
 
 // Example 2
-let obj : {name: string; isStudent: boolean} = {
-    name: "Abhijith",
-    isStudent: true
-}
+let obj: { name: string; isStudent: boolean } = {
+  name: "Abhijith",
+  isStudent: true,
+};
 // Here In the above example --> {name: string; isStudent: boolean} --> This is "Annotation"
 
-
 // Now for providing "our OWN name" to the "Annotation" --> We use 'Type Aliasing'
+
+// Type Aliasing
+
+type User = {
+  name: string;
+  age: number;
+  location: string;
+};
+
+const userInfo: User = {
+  name: "Abhijith",
+  age: 19,
+  location: "India",
+};
+
+const printUserInfo = (user: User) => {
+  return `Name: ${user.name} | Age: ${user.age} | Location: ${user.location}`;
+};
+const res = printUserInfo({ name: "Alex", age: 19, location: "USA" });
+console.log(res);
+
+
+
+// Method 1
+// let num: number = 7;
+
+// Method 2 - By using type Aliasing
+type myOwnName = number;
+let num: myOwnName = 11;
+
+console.log(num);
