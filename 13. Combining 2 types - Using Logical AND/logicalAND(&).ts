86b9514411b154d176b4cd125fa60c25c996465d
AbhijithKumar-07 <abhiyandrapu1106@gmail.com>
2025-06-27 11:42:51 +0530

    change

---
+++ 13. Combining 2 types - Using Logical AND/logicalAND(&).ts
@@ -0,0 +1,24 @@
+type UserInfo = {
+    first: string;
+    last: string;
+    age: number;
+}
+
+type AccountDetails = {
+    email: string;
+    password: string;
+}
+
+// For Combining 2 types - We use Logical AND Symbol- '&' - Ambressant 
+// type type_name = type_1 & type_2
+
+type user = UserInfo & AccountDetails;
+
+let user1: user = {
+    first: "HuXn",
+    last: "WebDEv",
+    age: 20,
+    email: "huxn@gmail.com",
+    password: "huxn@123"
+}
+
