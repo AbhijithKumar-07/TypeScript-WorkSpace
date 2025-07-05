aa4850214a11e478e9a130eb44726da972ef898d
AbhijithKumar-07 <abhiyandrapu1106@gmail.com>
2025-07-05 11:12:40 +0530

    Course Completed

--- 22.TypeScriptwithExpress/index.ts
+++ 22.TypeScriptwithExpress/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("<h1>Hello, TypeScript with Express🔗</h1>");
+  res.send("<h1>Hello :) TypeScript⭐ with ExpressJs🔗</h1>");
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
